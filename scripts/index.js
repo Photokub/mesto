31be62efdde9cf229380da775402d22c97c706cd
A_Kub_work <photokub@yandex.ru>
2022-06-30 19:08:29 +0300

    refactor: правки

--- scripts/index.js
+++ scripts/index.js
@@ -6,19 +6,19 @@ const submitBtn = popup.querySelector('.form__save-btn');
 const submitForm = popup.querySelector('.form');
 let userName = document.querySelector('.profile__title');
 let userJob = document.querySelector('.profile__subtitle');
-let userNameInput = popup.querySelector('.form__user-name');
-let userJobInput = popup.querySelector('.form__user-job');
+let userNameInput = popup.querySelector('.form__input-type-name');
+let userJobInput = popup.querySelector('.form__input-type-job');
 
 const likes = document.querySelectorAll('.element__like');
 
 const openPopupVisibility = function(){
     userNameInput.value = userName.textContent;
     userJobInput.value = userJob.textContent;
-    popup.classList.add('popup_is-opened');
+    popup.classList.add('popup_opened');
 };
 
 const closePopupVisibility = function(){
-    popup.classList.remove('popup_is-opened');
+    popup.classList.remove('popup_opened');
 };
 
 function formSubmit(evt){
